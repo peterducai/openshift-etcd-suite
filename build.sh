2ae6d74d72265ebd7eaad7e2b7ab9096fbecdefe
peter daemonna ducai <peter.ducai@gmail.com>
2022-03-09 14:45:27 +0100

    ignore

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 buildah bud -t quay.io/peterducai/openshift-etcd-suite:latest .
 podman tag quay.io/peterducai/openshift-etcd-suite:latest quay.io/peterducai/openshift-etcd-suite:0.1.4
-
+# podman login -u peterducai -p KZkM7Z+sIC5xhL43g4tQFjbo3VA+1MuBAA93xyZqLqv7Cumh6plykV0MGjJsjX0f quay.io
 # podman push quay.io/peterducai/openshift-etcd-suite:latest
 # podman push quay.io/peterducai/openshift-etcd-suite:0.1.3
 
