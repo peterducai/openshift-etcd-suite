8f2de05575ef8db45104797f6c902214e78778cc
peter daemonna ducai <peter.ducai@gmail.com>
2022-03-09 14:42:23 +0100

    remove runner

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 buildah bud -t quay.io/peterducai/openshift-etcd-suite:latest .
-podman login -u USER -p PASSWORD quay.io
+podman login -u peterducai -p KZkM7Z+sIC5xhL43g4tQFjbo3VA+1MuBAA93xyZqLqv7Cumh6plykV0MGjJsjX0f quay.io
 podman push quay.io/peterducai/openshift-etcd-suite:latest
 podman push quay.io/peterducai/openshift-etcd-suite:0.1.3
 
