2014f68c27b37ef2cd5ed3f31ae835090db62065
peter ducai <peter.ducai.dev@gmail.com>
2022-06-29 13:07:17 +0200

    fsync fio

--- fio_suite.sh
+++ fio_suite.sh
@@ -51,7 +51,24 @@ echo -e " "
 echo -e "- [SEQUENTIAL IOPS UNDER DIFFERENT READ/WRITE LOAD] ---"
 echo -e " "
 
-echo -e "-- [ SINGLE JOB, 70% read, 30% write] --"
+echo -e "[ETCD-like FSYNC WRITE with fsync engine]"
+echo -e ""
+echo -e "the 99th percentile of this metric should be less than 10ms"
+mkdir -p test-data
+/usr/bin/fio --rw=write --ioengine=sync --fdatasync=1 --directory=test-data --size=22m --bs=2300 --name=cleanfsynctest > cleanfsynctest.log
+FSYNC=$(cat cleanfsynctest.log |grep "99.00th"|tail -1|cut -c17-|grep -oE "([0-9]+)]" -m1|cut -d ']' -f 1|head -1)
+echo -e ""
+cat cleanfsynctest.log
+echo -e ""
+if (( $FSYNC > 10000 )); then
+    echo -e "BAD.. 99th fsync is higher than 10ms.  $FSYNC"
+else
+    echo -e "OK.. 99th fsync is less than 10ms.  $FSYNC"
+fi
+rm -rf test-data
+
+
+echo -e "-- [ libaio engine SINGLE JOB, 70% read, 30% write] --"
 echo -e " "
 
 /usr/bin/fio --name=seqread1g --filename=fiotest --runtime=120 --ioengine=libaio --direct=1 --ramp_time=10 --readwrite=rw --rwmixread=70 --rwmixwrite=30 --iodepth=1 --blocksize=4k --size=1G --percentage_random=0 > r70_w30_1G_d4.log
@@ -65,7 +82,7 @@ echo -e "$s7030small"
 rm r70_w30_200M_d4.log
 
 echo -e " "
-echo -e "-- [ SINGLE JOB, 30% read, 70% write] --"
+echo -e "-- [ libaio engine SINGLE JOB, 30% read, 70% write] --"
 echo -e " "
 
 /usr/bin/fio --name=seqwrite1G --filename=fiotest --runtime=120 --bs=2k --ioengine=libaio --direct=1 --ramp_time=10 --readwrite=rw --rwmixread=30 --rwmixwrite=70 --iodepth=1 --blocksize=4k --size=200M  > r30_w70_200M_d1.log
@@ -80,7 +97,7 @@ rm r30_w70_1G_d1.log
 rm fiotest
 
 echo -e " "
-echo -e "-- [ 8 PARALLEL JOBS, 70% read, 30% write] ----"
+echo -e "-- [ libaio engine 8 PARALLEL JOBS, 70% read, 30% write] ----"
 echo -e " "
 
 /usr/bin/fio --name=seqparread1g8 --filename=fiotest --runtime=120 --bs=2k --ioengine=libaio --direct=1 --ramp_time=10 --numjobs=8 --readwrite=rw --rwmixread=70 --rwmixwrite=30 --iodepth=1 --blocksize=4k --size=1G --percentage_random=0 > r70_w30_1G_d4.log
@@ -94,7 +111,7 @@ echo -e "$s7030small"
 rm r70_w30_200M_d4.log
 
 echo -e " "
-echo -e "-- [ 8 PARALLEL JOBS, 30% read, 70% write] ----"
+echo -e "-- [ libaio engine 8 PARALLEL JOBS, 30% read, 70% write] ----"
 echo -e " "
 
 /usr/bin/fio --name=seqparwrite1g8 --filename=fiotest --runtime=120 --bs=2k --ioengine=libaio --direct=1 --ramp_time=10 --numjobs=8 --readwrite=rw --rwmixread=30 --rwmixwrite=70 --iodepth=1 --blocksize=4k --size=200M  > r30_w70_200M_d1.log
