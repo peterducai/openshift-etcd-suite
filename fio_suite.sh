c26e2c41556e90b9472f3813ab400ef6b2e4ebf4
peter ducai <peter.ducai.dev@gmail.com>
2022-07-08 15:42:53 +0200

    cleanup

--- fio_suite.sh
+++ fio_suite.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "FIO SUITE version 0.1.24"
+echo -e "FIO SUITE version 0.1.26"
 echo -e " "
 echo -e "WARNING: this test can run for several minutes without any progress! Please wait until it finish!"
 echo -e " "
@@ -14,30 +14,30 @@ cd /test
 #       echo "fio is installed.. OK"
 # fi
 
-echo -e "- [MAX CONCURRENT READ] ---"
-echo -e "This job is a read-heavy workload with lots of parallelism that is likely to show off the device's best throughput:"
-echo -e " "
+# echo -e "- [MAX CONCURRENT READ] ---"
+# echo -e "This job is a read-heavy workload with lots of parallelism that is likely to show off the device's best throughput:"
+# echo -e " "
 
-/usr/bin/fio  --size=1G --name=maxoneg --filename=fiotest --runtime=120 --ioengine=libaio --direct=1 --ramp_time=10 --name=read1 --iodepth=16 --readwrite=randread --numjobs=16 --blocksize=64k --offset_increment=128m > best_1G_d4.log
-best_large=$(cat best_1G_d4.log |grep IOPS|tail -1)
-FSYNC=$(cat best_1G_d4.log |grep "99.00th"|tail -1|cut -c17-|grep -oE "([0-9]+)]" -m1|cut -d ']' -f 1|head -1)
-echo -e "1GB file:"
-echo -e "$best_large"
-echo -e "  99.00th fsync: $FSYNC"
-rm fiotest
-rm read*
-rm best_1G_d4.log
+# /usr/bin/fio  --size=1G --name=maxoneg --filename=fiotest --runtime=120 --ioengine=libaio --direct=1 --ramp_time=10 --name=test1 --iodepth=16 --readwrite=randread --numjobs=16 --blocksize=64k --offset_increment=128m > best_1G_d4.log
+# best_large=$(cat best_1G_d4.log |grep IOPS|tail -1)
+# FSYNC=$(cat best_1G_d4.log |grep "99.00th"|tail -1|cut -c17-|grep -oE "([0-9]+)]" -m1|cut -d ']' -f 1|head -1)
+# echo -e "1GB file:"
+# echo -e "$best_large"
+# echo -e "  99.00th fsync: $FSYNC"
+# rm fiotest
+# rm test*
+# rm best_1G_d4.log
 
-echo -e ""
-/usr/bin/fio --size=200M --name=maxonemb --filename=fiotest --runtime=120 --ioengine=libaio --direct=1 --ramp_time=10 --name=read1 --iodepth=16 --readwrite=randread --numjobs=16 --blocksize=64k --offset_increment=128m  > best_200M_d4.log
-best_small=$(cat best_200M_d4.log |grep IOPS|tail -1)
-FSYNC=$(cat best_200M_d4.log |grep "99.00th"|tail -1|cut -c17-|grep -oE "([0-9]+)]" -m1|cut -d ']' -f 1|head -1)
-echo -e "200MB file:"
-echo -e "$best_small"
-echo -e "  99.00th fsync: $FSYNC"
-rm fiotest
-rm read*
-rm best_200M_d4.log
+# echo -e ""
+# /usr/bin/fio --size=200M --name=maxonemb --filename=fiotest --runtime=120 --ioengine=libaio --direct=1 --ramp_time=10 --name=test2 --iodepth=16 --readwrite=randread --numjobs=16 --blocksize=64k --offset_increment=128m  > best_200M_d4.log
+# best_small=$(cat best_200M_d4.log |grep IOPS|tail -1)
+# FSYNC=$(cat best_200M_d4.log |grep "99.00th"|tail -1|cut -c17-|grep -oE "([0-9]+)]" -m1|cut -d ']' -f 1|head -1)
+# echo -e "200MB file:"
+# echo -e "$best_small"
+# echo -e "  99.00th fsync: $FSYNC"
+# rm fiotest
+# rm test*
+# rm best_200M_d4.log
 
 
 echo -e ""
@@ -45,26 +45,26 @@ echo -e "- [REQUEST OVERHEAD AND SEEK TIMES] ---"
 echo -e "This job is a latency-sensitive workload that stresses per-request overhead and seek times. Random reads."
 echo -e " "
 
-fio  --name=seek1g --filename=fiotest --runtime=120 --ioengine=libaio --direct=1 --ramp_time=10 --iodepth=4 --readwrite=randread --blocksize=4k --size=1G > rand_1G_d1.log
+fio --name=seek1g --filename=fiotest --runtime=120 --ioengine=libaio --direct=1 --ramp_time=10 --iodepth=4 --readwrite=randread --blocksize=4k --size=1G > rand_1G_d1.log
 overhead_big=$(cat rand_1G_d1.log |grep IOPS|tail -1)
 FSYNC=$(cat rand_1G_d1.log |grep "99.00th"|tail -1|cut -c17-|grep -oE "([0-9]+)]" -m1|cut -d ']' -f 1|head -1)
 echo -e "1GB file:"
 echo -e "$overhead_big"
 echo -e "  99.00th fsync: $FSYNC"
-rm fiotest
-rm read*
+# rm fiotest
+rm test*
 rm rand_1G_d1.log
 
 echo -e ""
-/usr/bin/fio  --name=seek1mb --filename=fiotest --runtime=120 --ioengine=libaio --direct=1 --ramp_time=10 --iodepth=4 --readwrite=randread --blocksize=4k --size=200M > rand_200M_d1.log
+/usr/bin/fio --name=seek1mb --filename=fiotest --runtime=120 --ioengine=libaio --direct=1 --ramp_time=10 --iodepth=4  --readwrite=randread --blocksize=4k --size=200M > rand_200M_d1.log
 overhead_small=$(cat rand_200M_d1.log |grep IOPS|tail -1)
 FSYNC=$(cat rand_200M_d1.log |grep "99.00th"|tail -1|cut -c17-|grep -oE "([0-9]+)]" -m1|cut -d ']' -f 1|head -1)
 echo -e "200MB file:"
 echo -e "$overhead_small"
 echo -e "  99.00th fsync: $FSYNC"
 rm rand_200M_d1.log
-rm fiotest
-rm read*
+# rm fiotest
+rm test*
 
 
 # echo -e " "
@@ -88,7 +88,7 @@ else
 fi
 echo -e ""
 rm -rf test-data
-rm read*
+#rm cleanfsynctest
 
 
 echo -e "-- [ libaio engine SINGLE JOB, 70% read, 30% write] --"
@@ -102,7 +102,7 @@ echo -e "$s7030big"
 echo -e "  99.00th fsync: $FSYNC"
 rm r70_w30_1G_d4.log
 rm fiotest
-rm read*
+# rm read*
 
 echo -e " "
 /usr/bin/fio --name=seqread1mb --filename=fiotest --runtime=120 --ioengine=libaio --direct=1 --ramp_time=10  --readwrite=rw --rwmixread=70 --rwmixwrite=30 --iodepth=1 --blocksize=4k --size=200M > r70_w30_200M_d4.log
@@ -113,7 +113,7 @@ echo -e "$s7030small"
 echo -e "  99.00th fsync: $FSYNC"
 rm r70_w30_200M_d4.log
 rm fiotest
-rm read*
+# rm read*
 
 echo -e " "
 echo -e "-- [ libaio engine SINGLE JOB, 30% read, 70% write] --"
@@ -127,7 +127,7 @@ echo -e "$so7030big"
 echo -e "  99.00th fsync: $FSYNC"
 rm r30_w70_200M_d1.log
 rm fiotest
-rm read*
+# rm read*
 
 echo -e " "
 /usr/bin/fio --name=seqwrite1mb --filename=fiotest --runtime=120 --bs=2k --ioengine=libaio --direct=1 --ramp_time=10 --readwrite=rw --rwmixread=30 --rwmixwrite=70 --iodepth=1 --blocksize=4k --size=1G > r30_w70_1G_d1.log
@@ -138,7 +138,7 @@ echo -e "$so7030small"
 echo -e "  99.00th fsync: $FSYNC"
 rm r30_w70_1G_d1.log
 rm fiotest
-rm read*
+# rm read*
 
 echo -e " "
 echo -e "-- [ libaio engine 8 PARALLEL JOBS, 70% read, 30% write] ----"
@@ -150,7 +150,7 @@ echo -e "1GB file:"
 echo -e "$s7030big"
 rm r70_w30_1G_d4.log
 rm fiotest
-rm read*
+# rm read*
 
 echo -e " "
 /usr/bin/fio --name=seqparread1mb8 --filename=fiotest --runtime=120 --bs=2k --ioengine=libaio --direct=1 --ramp_time=10 --numjobs=8 --readwrite=rw --rwmixread=70 --rwmixwrite=30 --iodepth=1 --blocksize=4k --size=200M > r70_w30_200M_d4.log
@@ -159,7 +159,7 @@ echo -e "200MB file:"
 echo -e "$s7030small"
 rm r70_w30_200M_d4.log
 rm fiotest
-rm read*
+# rm read*
 
 echo -e " "
 echo -e "-- [ libaio engine 8 PARALLEL JOBS, 30% read, 70% write] ----"
@@ -171,7 +171,7 @@ echo -e "1GB file:"
 echo -e "$so7030big"
 rm r30_w70_200M_d1.log
 rm fiotest
-rm read*
+# rm read*
 echo -e " "
 
 /usr/bin/fio --name=seqparwrite1mb8 --filename=fiotest --runtime=120 --bs=2k --ioengine=libaio --direct=1 --ramp_time=10 --numjobs=8 --readwrite=rw --rwmixread=30 --rwmixwrite=70 --iodepth=1 --blocksize=4k --size=1G > r30_w70_1G_d1.log
@@ -180,7 +180,7 @@ echo -e "200MB file:"
 echo -e "$so7030small"
 rm r30_w70_1G_d1.log
 rm fiotest
-rm read*
+# rm read*
 
 echo -e " "
 echo -e "- END -----------------------------------------"
--- push.sh
+++ push.sh
@@ -2,4 +2,4 @@
 
 podman login -u peterducai -p key quay.io
 podman push quay.io/peterducai/openshift-etcd-suite:latest
-podman push quay.io/peterducai/openshift-etcd-suite:0.1.24
+podman push quay.io/peterducai/openshift-etcd-suite:0.1.26
